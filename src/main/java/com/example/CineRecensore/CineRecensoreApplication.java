45d0b859b3df7c6776b59af9488b0e451b6d7a5c
Sall <salvatoreparisi97@gmail.com>
2023-10-05 12:14:05 +0200

    Aggiunto Progetto base.

---
+++ src/main/java/com/example/CineRecensore/CineRecensoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.CineRecensore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineRecensoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineRecensoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CineRecensore/CineRecensoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CineRecensore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineRecensoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
